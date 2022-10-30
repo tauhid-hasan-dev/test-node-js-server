d50a4202e58e67a1acc0ade1f3b9832dca162120
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-10-30 10:53:24 +0300

    mongodb connected to this app

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
-
-
 
 app.use(cors());
 app.use(express.json());
@@ -16,12 +15,33 @@ const users = [
     { id: 4, name: 'Tazdid Hasan', email: 'tazdidhasan11@gmail.com' },
 ]
 
+//username: dbUser2
+//password : WAP4KDVKgS3EhEYT
+
+
+const uri = "mongodb+srv://dbUser2:WAP4KDVKgS3EhEYT@cluster0.jjvuikj.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("testserver").collection("users");
+    // perform actions on the collection object
+    console.log('database connected');
+    client.close();
+});
+
+
 app.get('/', (req, res) => {
     res.send('Test server is running');
 })
 
 app.get('/users', (req, res) => {
-    res.send(users)
+    console.log(req.query)
+    if (req.query.name) {
+        const search = req.query.name;
+        const filtered = users.filter(usr => usr.name.toLowerCase().indexOf(search) >= 0)
+        res.send(filtered);
+    } else {
+        res.send(users)
+    }
 })
 
 app.post('/users', (req, res) => {
